3e48661215f38293bc174912b8924cdbb560ebb2
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-07-09 16:19:19 +0500

    Add files via upload

---
+++ InterpolationPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <!-- <h1>Name {{10+20 }}</h1>  basic concept of nterpolation-->
+    <div class="container"> 
+        <h3>Name = {{getName() }}</h3>
+        <!-- also send name from here to function(param) ret param -->
+        <h3>Contact No = {{Contact=3165392191}}</h3>
+        <!-- in vue we can change variable value but ! angular -->
+        <h3>Qualification = {{q }}</h3>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'InterpolationPage', // this is the component's name, which will be used in templates
+    data() {
+        return {
+            Contact : "98765432",
+            q :"B.Tech",
+            getName:function(){
+                return "Nasir Hussain"
+            }
+        }
+    },
+}
+</script>
