56a27034ce4340696b6191e77098d5fb5540372d
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-07-17 22:21:33 +0500

    Add files via upload

---
+++ Parent & Child component/ChildPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <h2>This is child Page</h2>
+    <h4>{{ name }}</h4>
+    <h4>{{user.name}}</h4>
+    <button v-on:click="getEmail()">CLick Me</button>
+
+</template>
+<script>
+export default{
+    name: "ChildPage",
+    props:{
+        name:String,
+        user:Object,
+        getEmail:Function,   
+    }
+}
+
+</script>
---
+++ Parent & Child component/ParentPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>Passing Data to Child Data</h1>
+    <ChildPage :name="name" :user="user" :getEmail="getEmail"/>
+</template>
+<script>
+import ChildPage from './ChildPage.vue';
+export default{
+    name:'ParentPage',
+    components: {
+        // 'ChildPage': ChildPage, //register the child component in this parent page's scope
+        ChildPage  //shorthand syntax for above line of code
+    },
+    data(){
+        return{
+            name:"Nahida Batool",
+            user:{name:"Kashif",email:"kashif@gmail.com"}
+        }
+    },
+    methods: {
+        getEmail(){
+            alert(this.user.email)
+        }
+    },
+}
+</script>
