195deb90a7f1d3de0b4a341558d0c9144aee5a96
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-07-18 12:07:51 +0500

    Add files via upload

---
+++ Component Reuse/HomePage.vue
@@ -0,0 +1,43 @@
+<template>
+    <h1>Reuse of Compononets</h1>
+    <div class="container">
+        <ul>
+            <li v-for="item in user" :key="item">
+                <UserPage :data="item" :getData="getData"/>
+                <!-- {{item.name }} -->
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import UserPage from './UserPage.vue'
+export default {
+    name: "HomePage",
+    components: {
+        UserPage
+    },
+    methods:{
+        getData(name){
+            alert(name);
+        }
+    },
+    data() {
+        return {
+            user: [
+                { name: "Nasir", email: "nasir@g.com" },
+                { name: "Yasir", email: "yasir@g.com" },
+                { name: "Basir", email: "basir@g.com" },
+                { name: "Qasir", email: "qasir@g.com" },
+            ]
+        }
+    },
+  
+}
+</script>
+<style>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
---
+++ Component Reuse/UserPage.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h3> {{ data.name }}</h3>
+        <h3>{{ data.email }}</h3>
+        <button v-on:click="getData(data.name)">Click Me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'UserPage',
+    props:{
+        data:Object,
+        getData : Function //function to get the user details from API or any other source
+    },
+   
+}
+</script>
