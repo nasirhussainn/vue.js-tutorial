4cb65570049f8116a0c63816c89b2c5b3cd6fbec
Nasir Hussain <95434256+nasirhussainn@users.noreply.github.com>
2023-08-06 17:39:48 +0500

    Add files via upload

---
+++ Ref.vue
@@ -0,0 +1,18 @@
+<template>
+    <h3>Refs in Vue.js</h3>
+    <input type="text" name="ref" id="myref" ref="input">
+    <button v-on:click="getData()">Click Me</button>
+</template>
+<script>
+export default {
+    name:"RefVue",
+    methods:{
+        getData(){
+            let val=this.$refs.input.value
+            alert(val)
+            this.$refs.input.focus()
+            this.$refs.input.style.color="red"
+        }
+    }
+}
+</script>
